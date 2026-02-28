c6f18d8139c26969e4fd69909877413eb62c473e
santhu <santhoshnerella7@gmail.com>
2026-02-28 18:22:40 +0530

    smallest num in arr when arr given in special type

--- SmallestNum.java
+++ SmallestNum.java
@@ -14,17 +14,41 @@ public class SmallestNum {
             // }
             // System.out.println("the Smallest elemnet in array is :" + min);
             //---------------------------------------------------------------------------------------------------------------------------
+            // String line = sc.nextLine();
+            // String[] tokens = line.split("\\s+");
+            // List<Integer> arr = new ArrayList<>();
+            // for(String token : tokens){                                      //when  arr  size is not given
+            //     arr.add(Integer.parseInt(token));
+            // }
+            // int min = Integer.MAX_VALUE;
+            // for(int num : arr){
+            //     if(num < min) min = num;
+            // }
+            // System.out.println("Smallest number in arr is :" + min);
+            //---------------------------------------------------------------------------------------------------------------------------------
             String line = sc.nextLine();
-            String[] tokens = line.split("\\s+");
             List<Integer> arr = new ArrayList<>();
-            for(String token : tokens){
-                arr.add(Integer.parseInt(token));
+            StringBuilder num = new StringBuilder();
+            for(char ch : line.toCharArray()){
+                if(Character.isDigit(ch)){
+                    num.append(ch);
+                }else{
+                    if(num.length() > 0){
+                        arr.add(Integer.parseInt(num.toString()));
+                        num.setLength(0);
+                    }
+                }
+            }
+            if(num.length() > 0){
+                arr.add(Integer.parseInt(num.toString()));
             }
             int min = Integer.MAX_VALUE;
-            for(int num : arr){
-                if(num < min) min = num;
+            for(int ar : arr){
+                if(ar < min) min = ar;
             }
-            System.out.println("Smallest number in arr is :" + min);
+            System.out.println("smallest num is : " + min);
+
+
     }
     
 }
