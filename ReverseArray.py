bcfd05f14b57998ec8ca4c9502ab127a7e14303c
santhu <santhoshnerella7@gmail.com>
2026-03-04 18:16:50 +0530

    Revrsing array elemnts uing two pointr apprch

--- ReverseArray.py
+++ ReverseArray.py
@@ -11,5 +11,13 @@ arr = list(map(int, input().split()))
 # reverse_array = arr[::-1]
 # print(reverse_array)
 #------------------------------------------------------------------------------------
-reversed_arr = list(reversed(arr))
-print(reversed_arr)
+# reversed_arr = list(reversed(arr))
+# print(reversed_arr)
+#-------------------------------------------------------------------------------------
+p1 = 0
+p2 = len(arr) - 1
+while p1 < p2 :
+    arr[p1] , arr[p2] = arr[p2] , arr[p1]
+    p1 +=1
+    p2 -=1
+print(arr)
