4e2eaf5e9b71072b24aaa5342c53676acabac7d3
santhu <santhoshnerella7@gmail.com>
2026-03-04 18:10:37 +0530

    Reversing array elemnts in place

---
+++ ReverseArray.py
@@ -0,0 +1,15 @@
+arr = list(map(int, input().split()))
+#-------------------------------------------------------------------
+# arr.reverse()
+# print(arr)
+#-----------------------------------------------------------------------
+# reverse_Arr = []
+# for i in range (len(arr) - 1 , -1 , -1):
+#     reverse_Arr.append(arr[i])
+# print(reverse_Arr)
+#-------------------------------------------------------------------------------
+# reverse_array = arr[::-1]
+# print(reverse_array)
+#------------------------------------------------------------------------------------
+reversed_arr = list(reversed(arr))
+print(reversed_arr)
