386cddce1a8746f881edba115ce95f32de6e14af
santhu <santhoshnerella7@gmail.com>
2026-03-21 19:08:50 +0530

    MAx&Min digit in a number

---
+++ Max_Min_digits.py
@@ -0,0 +1,13 @@
+n = int(input())
+max_digit = 0
+min_digit = 9
+while n > 0 :
+    digit = n % 10 
+    if digit > max_digit :
+        max_digit = digit
+    if digit < min_digit :
+        min_digit = digit
+    n //= 10
+print([max_digit,min_digit])
+print(max_digit,min_digit)
+print((max_digit,min_digit))
