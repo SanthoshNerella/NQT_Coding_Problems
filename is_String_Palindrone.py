69d581fbf5b2f0ca6faf73961ba8a64e6a8e55ac
santhu <santhoshnerella7@gmail.com>
2026-03-23 17:58:34 +0530

    is_String palindrone or not

---
+++ is_String_Palindrone.py
@@ -0,0 +1,19 @@
+s = input()
+def is_palindrone(s):
+    n = len(s)
+    l = 0
+    r = n - 1
+    while l < r :
+        if not s[l].isalnum():
+            l += 1
+            continue
+        if not s[r].isalnum():
+            r -= 1
+            continue
+        if s[l].lower() != s[r].lower():
+            return False
+        l += 1
+        r -= 1
+    return True
+print(is_palindrone(s))
+
